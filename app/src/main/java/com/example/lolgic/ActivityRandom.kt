c3d4b6a65eb7f06e66776c93f516bee10a4dac7c
CalvinVirya <calvinviryayunardy@gmail.com>
2025-06-05 12:57:56 +0700

    logic pindah page

--- app/src/main/java/com/example/lolgic/ActivityRandom.kt
+++ app/src/main/java/com/example/lolgic/ActivityRandom.kt
@@ -2,6 +2,7 @@ package com.example.lolgic
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +15,7 @@ import com.android.volley.toolbox.Volley
 class ActivityRandom : AppCompatActivity() {
 
     lateinit var tvApi: TextView
+    lateinit var tvTitle: TextView
     private lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,23 +24,33 @@ class ActivityRandom : AppCompatActivity() {
         setContentView(R.layout.activity_random)
 
         tvApi = findViewById(R.id.tvApi)
+        tvTitle = findViewById(R.id.tvTitle)
 
-        tvApi.text = intent.getStringExtra("ActivityName")
+        tvTitle.text = intent.getStringExtra("ActivityName")
 
-//        requestQueue = Volley.newRequestQueue(this)
-//
-//        val url = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
-//
-//        var request = JsonObjectRequest(
-//            Request.Method.GET, url, null,
-//            { response ->
-//                tvApi.text = response.getString("text")
-//            },
-//            { error ->
-//                error.printStackTrace()
-//            }
-//        )
-//        requestQueue.add(request)
+        var url: String
+
+        if(tvTitle.text == "Useless Facts") {
+            url = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+        } else {
+            Toast.makeText(this, "No API available for: ${tvApi.text}", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+
+        requestQueue = Volley.newRequestQueue(this)
+
+        var request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                tvApi.text = response.getString("text")
+            },
+            { error ->
+                tvApi.text = "API not found"
+            }
+        )
+        requestQueue.add(request)
 
     }
 }
